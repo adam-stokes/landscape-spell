e53c97e2fd4ab8c6e3cadd2302f6ff7af8dd0742
Adam Stokes <battlemidget@users.noreply.github.com>
2017-03-16 19:12:02 -0400

    booyah
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- steps/setup-landscape.sh
+++ steps/setup-landscape.sh
@@ -1,8 +1,5 @@
 sudo apt-get -qq update && sudo apt-get -qqyf install landscape-api
 landscape-api call BootstrapLDS --json admin_email="$LDS_EMAIL" admin_name="$LDS_NAME" admin_password="$LDS_PASSWORD" --uri https://$HAPROXY/api/ --ssl-ca-file /etc/ssl/certs/landscape_server_ca.crt > /dev/null 2>&1
-RET=$?
-
-exit $RET
 # export LANDSCAPE_API_KEY=$(echo $CREDS|python -c "import sys,yaml; print(yaml.load(sys.stdin))['LANDSCAPE_API_KEY']")
 # export LANDSCAPE_API_SECRET=$(echo $CREDS|python -c "import sys,yaml; print(yaml.load(sys.stdin))['LANDSCAPE_API_SECRET']")
 # Dont register against MAAS until AutoPilot works with MAAS v2+
